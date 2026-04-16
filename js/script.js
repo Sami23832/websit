b08716ab6c38f99e794d068044693c1fecdf0323
Samuel <samuelgudisa97@gmail.com>
2026-04-16 09:43:00 +0300

    this is my first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*  ======= typing animation =======*/
+var typed = new Typed(".typing",{
+    String:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
